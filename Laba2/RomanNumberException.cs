8c37f929d569bbb214178f6a1301ce691bc2e7ef
Vladimir <vladimir-malyschevvv@mail.ru>
2022-02-24 21:10:09 +0700

    implemented romannumbers

---
+++ Laba2/RomanNumberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+
+{
+    public class RomanNumberException : Exception
+    {   
+        public RomanNumberException() : base() { }
+        public RomanNumberException(string message) : base(message) { }
+    }
+}
